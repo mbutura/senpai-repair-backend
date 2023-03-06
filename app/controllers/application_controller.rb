f8090c4766bc32ddf5c4fa423c47c3abbeb69b10
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-03-06 03:42:12 +0000

    update controller with singular item GET methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,24 @@ class ApplicationController < Sinatra::Base
                               include: {jobs: {only: [:id, :latitude, :longitude, :description]}})
   end
 
+  #Used when displaying in a single entry
+  get "/service_people/:id" do
+    service_person = ServicePerson.find(params[:id])
+    service_person.to_json(only: [:id, :first_name, :last_name],
+                              include: {jobs: {only: [:id, :latitude, :longitude, :description]}})
+  end
+
   #Used when displaying in a list
   get "/jobs" do
     Job.all.to_json(only: [:id, :latitude, :longitude, :description], 
+                    include: {job_status:{ only: [:category]},
+                              service_person:{ only: [:first_name, :last_name]}})
+  end
+
+  #Used when displaying a single entry
+  get "/jobs/:id" do
+    job = Jobs.find(params[:id])
+    job.to_json(only: [:id, :latitude, :longitude, :description], 
                     include: {job_status:{ only: [:category]},
                               service_person:{ only: [:first_name, :last_name]}})
   end
